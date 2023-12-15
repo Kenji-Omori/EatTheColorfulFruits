05e82c8b5b3a80e1c215a4ba342b59edcb49750e
OomoriKenji <oomoriknj@g.neec.ac.jp>
2023-12-15 17:00:11 +0900

    Vector2などのKamataEngine由来のクラスをプロジェクトへ移動

---
+++ Vector2.cpp
@@ -0,0 +1,63 @@
+#include "Vector2.h"
+#include <math.h>
+
+Vector2 Vector2::Normalize() { 
+	float length = this->Length(); 
+	if (length <= 0) {
+		return {0,0};
+	}
+	Vector2 result{
+		x/length,
+		y/length,
+	};
+	return result;
+}
+
+float Vector2::Length() { return sqrtf(LengthSqr()); }
+
+float Vector2::LengthSqr() { return x*x+y*y; }
+
+float Vector2::DistanceSqr(Vector2 other) 
+{ 
+	Vector2 def{
+		other.x - x,
+		other.y - y
+	};
+	return def.LengthSqr(); 
+}
+
+float Vector2::Distance(Vector2 other) { 
+	return sqrtf(this->DistanceSqr(other));
+}
+
+Vector2 Vector2::operator+(Vector2 other) {
+	Vector2 result{
+	   x + other.x,
+	   y + other.y
+	};
+  return result;
+}
+
+Vector2 Vector2::operator-(Vector2 other) { 
+	Vector2 result{
+	   x - other.x,
+	   y - other.y
+	};
+  return result;
+}
+
+Vector2 Vector2::operator*(float other) { 
+	Vector2 result{
+	   x * other,
+	   y * other
+	};
+  return result;
+}
+
+Vector2 Vector2::operator/(float other) { 
+
+	Vector2 result{x / other, y / other};
+  return result;
+}
+
+
---
+++ Vector2.h
@@ -0,0 +1,18 @@
+#pragma once
+
+/// <summary>
+/// 2次元ベクトル
+/// </summary>
+struct Vector2 final {
+	float x;
+	float y;
+	Vector2 Normalize();
+	float Length();
+	float LengthSqr();
+	float DistanceSqr(Vector2 other);
+	float Distance(Vector2 other);
+	Vector2 operator+(Vector2 other);
+	Vector2 operator-(Vector2 other);
+	Vector2 operator*(float other);
+	Vector2 operator/(float other);
+};
